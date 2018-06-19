a359340e7eb0b45d76efc9d9d50cc557d44ae6b0
Amir Massarwi <amassarwi@gmail.com>
2018-06-20 01:27:17 +0200

    Add http call to get artist info by name

--- src/common/data.service.ts
+++ src/common/data.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class DataService {
-    constructor() { }
+
+    private appId: string;
+    private baseUrl: string;
+
+    constructor(
+        private http: HttpClient
+    ) {
+        this.appId = 'thiscanbeanystring';
+        this.baseUrl = 'https://rest.bandsintown.com';
+    }
+
+    getArtistInfoByName(artistName: string) {
+        return this.http.get(`${this.baseUrl}/artists/${artistName}`);
+    }
 }
